50ff295c973898515efb49f99d099067ee7c2b6b
lghchimsarah21 <lghchim.sarah@gmail.com>
2025-12-14 15:22:19 +0100

    commited again

--- src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello() {
-        return "Hello from Different New Test :)";
+        return "Hello from Different  Test :)";
     }
 
     @GetMapping("/user")
